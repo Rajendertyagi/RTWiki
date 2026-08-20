470c8a2656e5feecc9c25e293ec62cf3186f5654
RTWiki <rtwiki@rtwiki.local>
2026-08-21 04:00:16 +0530

    chore: try alternative JSX global declaration

--- src/web/jsx.d.ts
+++ src/web/jsx.d.ts
@@ -1,12 +1,17 @@
 /// <reference types="vite/client" />
 
-// React 19 + TypeScript 7 requires explicit JSX namespace import.
-// This global declaration makes JSX.Element available without importing it in every file.
+// React 19 + TypeScript 7 compatibility layer.
+// React 19 moved JSX types out of the global namespace.
+// This module Augments the global JSX namespace using React 19's exported types.
+import type * as React from 'react'
+
+// Create a local alias to avoid redeclaration issues
+type _ReactJSX = typeof React extends { default: { JSX: infer T } } ? T : never
+
+// Declare the global JSX namespace
 declare global {
-  // eslint-disable-next-line @typescript-eslint/no-namespace
-  namespace JSX {
-    interface Element extends React.JSX.Element {}
-    interface ElementClass extends React.JSX.ElementClass {}
-    interface IntrinsicElements extends React.JSX.IntrinsicElements {}
-  }
+  // eslint-disable-next-line no-var
+  var _jsxNamespace: _ReactJSX
 }
+
+export {}
