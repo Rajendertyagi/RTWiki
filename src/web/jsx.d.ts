7fdc2abca0986a18117d94a1c6eeb73abfcbeca0
RTWiki <rtwiki@rtwiki.local>
2026-08-21 03:52:10 +0530

    fix: rewrite JSX global declaration to avoid lint errors

--- src/web/jsx.d.ts
+++ src/web/jsx.d.ts
@@ -2,13 +2,11 @@
 
 // React 19 + TypeScript 7 requires explicit JSX namespace import.
 // This global declaration makes JSX.Element available without importing it in every file.
-import type { JSX } from 'react'
-
 declare global {
   // eslint-disable-next-line @typescript-eslint/no-namespace
   namespace JSX {
-    export { JSX }
+    interface Element extends React.JSX.Element {}
+    interface ElementClass extends React.JSX.ElementClass {}
+    interface IntrinsicElements extends React.JSX.IntrinsicElements {}
   }
 }
-
-export {}
