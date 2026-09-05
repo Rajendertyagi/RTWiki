be5c250cd29aab4099025e9ec35e5baae0d73d70
Rajendertyagi <Rajendertyagi@users.noreply.github.com>
2026-09-05 23:25:59 +0530

    refactor: remove proven unused modules

--- src/shared/constants/pages.ts
+++
@@ -1,4 +0,0 @@
-// The autosave debounce is defined once in constants/index.ts
-// (PROVISIONAL_AUTOSAVE_DEBOUNCE_MS); never re-declare it here.
-export const MAX_TITLE_LENGTH = 200
-export const DEFAULT_PAGE_TYPE = 'rich' as const
